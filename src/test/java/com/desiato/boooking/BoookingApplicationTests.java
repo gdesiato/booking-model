25ae8289ff8b811bc042f234fc2075937e23be2e
Giuseppe Desiato <giodesiato@gmail.com>
2025-02-10 19:55:34 +0100

    resources

---
+++ src/test/java/com/desiato/boooking/BoookingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desiato.boooking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
